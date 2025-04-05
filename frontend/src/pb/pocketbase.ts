ba6bc01b5ef01ffae8f3c82fa1c09516ba879761
bosscerise <212137069315@etu.univ-usto.dz>
2025-04-06 00:50:06 +0100

    testing

--- frontend/src/pb/pocketbase.ts
+++ frontend/src/pb/pocketbase.ts
@@ -3,4 +3,11 @@ import PocketBase from 'pocketbase';
 // Create a single PocketBase instance for the entire app
 const pb = new PocketBase('https://597d-2a09-bac5-3071-1a78-00-2a3-17.ngrok-free.app');
 pb.autoCancellation(false);
+pb.beforeSend = (url: string, options: RequestInit) => {
+  options.headers = {
+      ...options.headers,
+      'ngrok-skip-browser-warning': 'true'
+  };
+  return { url, options };
+};
 export default pb;
